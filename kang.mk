b6633df7ad8dd4f352c04fd8cf1c6c3d064478a8
Emmanuel Utomi <emmanuelutomi@gmail.com>
2013-05-05 20:17:20 -0500

    JellyKang v1.7

--- kang.mk
+++ kang.mk
@@ -1,6 +1,6 @@
 # JellyKang
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kang.version=V1.6-Doubleshot-$(shell date +%y%m%d) \
+    ro.kang.version=V1.7-Doubleshot-$(shell date +%y%m%d) \
     ro.goo.developerid=xmcwildchild22 \
     ro.goo.board=doubleshot \
     ro.goo.version=$(shell date +%y%m%d) \
